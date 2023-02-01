0d97bc4edc527db68bf6a54570cfc7589d0d7791
Victor Leonov <victorlev0878@gmail.com>
2023-02-01 22:17:04 +0700

    initial commit

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
-per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
-money = int(input("Введите сумму вклада: "))
-val_per = list(per_cent.values())
-t, s,  v, sb = val_per[0], val_per[1], val_per[2], val_per[3]
-t = int(val_per[0]*money / 100)
-s = int(val_per[1]*money / 100)
-v = int(val_per[2]*money / 100)
-sb = int(val_per[3]*money / 100)
-deposit = [val_per]
-num = int()
-profit = money * num
-print(val_per)
-print(t, s, v, sb)
-print("Максимальная сумма, которую вы можете заработать: ", max([t, s, v, sb]))
+tickets = int(input("При покупке от 3-х билетов действует скидка 10% от стоимости заказа!\n"
+                   "Введите количество билетов: "))
+age = list(map(int, input("Укажите через пробел возраст посетителей: ").split()))
+while tickets != len(age):
+    age = list(map(int, input("Количество посетителей не совпадает с количеством билетов.\n"
+                              "Укажите через пробел возраст посетителей: ").split()))
+price = []
+for i in age:
+    if i in range(0, 18):
+        price.append(0)
+    elif i in range(18, 25):
+        price.append(990)
+    else:
+        price.append(1390)
+if tickets >= 3:
+    print("Сумма к оплате с учетом скидки: ", sum(price) - ((sum(price) / 100) * 10), "рублей")
+else:
+    print("Сумма к оплате: ", sum(price), "рублей")
